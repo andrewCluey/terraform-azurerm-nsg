8e2b26bd961e75da37a9853fd0d05ef3b29883b3
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-04-28 11:19:50 +0100

    added nsg name output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "nsg_id" {
   description = "The ID of the newly created Network Security Group"
   value       = azurerm_network_security_group.nsg.id
 }
+
+output "nsg_name" {
+  description = "The name of the new NSG"
+  value       = azurerm_network_security_group.nsg.name
+}
